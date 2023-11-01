64f788b3101f12b769fb57e331ee7ec5918d1d48
11Hamza <hamzaaebrahim@gmail.com>
2023-11-01 10:07:58 +0200

    test commit

--- app/src/main/java/com/wordapp/test2/Recipient/FinalizeRequestsActivity.kt
+++ app/src/main/java/com/wordapp/test2/Recipient/FinalizeRequestsActivity.kt
@@ -1,14 +1,18 @@
 package com.wordapp.test2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class FinalizeRequestsActivity : AppCompatActivity() {
 
@@ -27,10 +31,47 @@ class FinalizeRequestsActivity : AppCompatActivity() {
 
         val confirmButton: Button = findViewById(R.id.confirmButton)
         confirmButton.setOnClickListener {
-            RequestTracker.finalizeRequests()  // Finalize the requests when Confirm button is clicked
-        }
+            finalizeRequests()
 
+        }
     }
+
+
+
+    private fun finalizeRequests() {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val userId = currentUser?.uid ?: return  // Get the current user's uid
+
+        val dbRef = FirebaseDatabase.getInstance().getReference("users").child(userId)
+        val requestsRef = dbRef.child("requests")
+        val finalizedRequestRef = dbRef.child("finalizedRequests").push()  // Create a new child node for the finalized request
+
+        requestsRef.get().addOnSuccessListener { snapshot ->
+            val requests = snapshot.children.mapNotNull { it.getValue(RecipientActivity.RequestItem::class.java) }
+            if (requests.isNotEmpty()) {
+                finalizedRequestRef.setValue(requests).addOnSuccessListener {
+                    // If the copy was successful, delete the individual requests
+                    requestsRef.removeValue().addOnSuccessListener {
+                        Toast.makeText(this, "Request finalized successfully.", Toast.LENGTH_SHORT).show()
+
+                        // Navigate to OngoingRequestsActivity upon successful finalization
+                        val intent = Intent(this, OngoingRequestsActivity::class.java)
+                        startActivity(intent)
+
+                    }.addOnFailureListener {
+                        Toast.makeText(this, "Failed to remove old requests: ${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Failed to finalize request: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "No requests to finalize.", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to retrieve requests: ${it.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     inner class FinalizeAdapter(private val items: List<RecipientActivity.RequestItem>) :
         RecyclerView.Adapter<FinalizeAdapter.ViewHolder>() {
---
+++ app/src/main/java/com/wordapp/test2/Recipient/OngoingRequestsActivity.kt
@@ -0,0 +1,86 @@
+package com.wordapp.test2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+class OngoingRequestsActivity : AppCompatActivity() {
+
+    private lateinit var databaseReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_going_requests)
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().currentUser?.uid ?: return).child("finalizedRequests")
+
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_on_going_requests)
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("users")
+                .child(FirebaseAuth.getInstance().currentUser?.uid ?: return)
+                .child("finalizedRequests")
+
+            setupRecyclerView()  // Call this method to set up the RecyclerView
+        }
+
+
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val requestsList = mutableListOf<Request>()
+                for (requestSnapshot in dataSnapshot.children) {
+                    val itemsList = mutableListOf<RecipientActivity.RequestItem>()
+                    for (itemSnapshot in requestSnapshot.children) {
+                        val item = itemSnapshot.getValue(RecipientActivity.RequestItem::class.java)
+                        if (item != null) {
+                            itemsList.add(item)
+                        }
+                    }
+                    val request = Request(id = requestSnapshot.key ?: "", items = itemsList)
+                    requestsList.add(request)
+                }
+                recyclerView.adapter = RequestsAdapter(requestsList)
+            }
+
+        })
+    }
+
+    data class Request(
+        val id: String = "",
+        val items: List<RecipientActivity.RequestItem> = listOf()
+    )
+
+    inner class RequestsAdapter(private val requests: List<Request>) : RecyclerView.Adapter<RequestsAdapter.ViewHolder>() {
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val itemName: TextView = view.findViewById(R.id.item_name)  // Assuming each item has a name to be displayed
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.ongoingreqview, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val request = requests[position]
+            // Assuming each item has a name to be displayed
+            holder.itemName.text = request.items.joinToString(", ") { it.name }
+        }
+
+
+        override fun getItemCount() = requests.size
+    }
+}
